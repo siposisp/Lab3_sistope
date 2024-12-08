661eb8bc2ee76dbf0d664ac8e243ec48a59df0e4
Nicolas Gajardo Ponce <nicolasgajardo11@gmail.com>
2024-12-08 04:06:19 -0300

    Funcionando, pero con problemas

--- lab3.c
+++ lab3.c
@@ -5,9 +5,14 @@
 #include <getopt.h>
 #include <string.h>
 
+pthread_mutex_t lock; // Los mutex se ocupan como variable global
+int* A;               // Arreglo global compartido
+int cantidad_numeros; // Tamaño del arreglo
+char* debug;
+
 
 // Función para llenar un arreglo con los números del archivo.
-int* readFileToArray(const char* filename, int size) {
+int* readFileToArray(char* filename, int size) {
     FILE* file = fopen(filename, "r");
     if (!file) {
         perror("Error al abrir el archivo");
@@ -44,8 +49,8 @@ void vaciar_archivo(char *nombreArchivo){
     fclose(archivo);
 }
 
-int largo_arreglo(char* filename){
-    
+// Cuenta cuántos números hay en el archivo
+int largo_arreglo(char* filename) {
     FILE* file = fopen(filename, "r");
     if (!file) {
         perror("Error al abrir el archivo");
@@ -54,12 +59,10 @@ int largo_arreglo(char* filename){
 
     int tamano = 0;
     int temp;
-
-        // Cuenta los numeros leyendo linea por linea.
     while (fscanf(file, "%d", &temp) == 1) {
         tamano++;
     }
-
+    fclose(file); // Cierra correctamente el archivo
     return tamano;
 }
 
@@ -82,16 +85,70 @@ void guardar_en_archivo(char *nombreArchivo, int* arreglo, int largo){
 }
 
 
+
+// Función que ejecuta cada hebra
+void* funcion(void* arg) {
+    // Extraer la posición y liberar memoria
+    int posicion = *(int*)arg;
+    free(arg);
+
+    // Actualizar el valor en el arreglo global
+    pthread_mutex_lock(&lock); // Bloquear el acceso al recurso compartido para evitar condicion de carrera
+    if (A[posicion] < A[posicion + 1]) {
+        A[posicion] = A[posicion + 1]; // Asignar el mayor valor
+    }
+
+    if(posicion == cantidad_numeros-1){
+        if(A[posicion] < A[0]){
+            A[posicion] = A[0];
+        }
+    }
+
+    // Imprimir por pantalla para hacer "debug"
+    if (strcmp(debug, "debug") == 0) {
+        printf("Contenido del arreglo:[");
+        for (int i = 0; i < cantidad_numeros; i++) {
+            printf("%d ", A[i]);
+        }
+        printf("]\n");
+    }
+
+    pthread_mutex_unlock(&lock); // Desbloquear el recurso compartido
+
+    return NULL;
+}
+
+
+// Funcion que verifica si todos los elementos dentro de un arreglo son iguales
+int iguales(int arreglo[], int tamano) {
+    // Si el tamaño es 0 o 1, consideramos que todos son iguales
+    if (tamano <= 1) {
+        return 1;
+    }
+
+    // Comparamos todos los elementos con el primero
+    for (int i = 1; i < tamano; i++) {
+        if (arreglo[i] != arreglo[0]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
 //Bloque Principal
 int main(int argc, char *argv[])
 {
     printf("****************INICIO DEL PROGRAMA****************\n\n");
 
+    // Se inicia el mutex
+    pthread_mutex_init(&lock, NULL);
+
     // Variables para almacenar las opciones de línea de comandos
     int option;
     char *archivoentrada = NULL;
     char *archivosalida = NULL;
-    char* debug = NULL;
+    debug = NULL;
 
     //Se utiliza geopt para leer las opciones de línea de comandos
     while ((option = getopt(argc, argv, "i:o:D")) != -1) {
@@ -126,10 +183,33 @@ int main(int argc, char *argv[])
     }
 
     // Contamos la cantidad de numeros en el arreglo
-    int cantidad_numeros = largo_arreglo(archivoentrada);
+    cantidad_numeros = largo_arreglo(archivoentrada);
     
-    
-    int* A = readFileToArray(archivoentrada, cantidad_numeros); // Arreglo con los numeros de entrada
+    // Transformamos los numeros del archivo en un arreglo
+    A = readFileToArray(archivoentrada, cantidad_numeros); // Arreglo con los numeros de entrada
+
+    int listo = 0; //Variable para controlar cuando todos los elementos del arreglo sean iguales
+    while(!listo){
+        // Creamos N hebras
+        pthread_t tids[cantidad_numeros];
+        for(int i=0;i<cantidad_numeros;i++){
+            int* posicion = malloc(sizeof(int)); // Reservar memoria para el número
+            *posicion = i;
+            //printf("i: %d, contenido arreglo: %d\n", i, A[i]);
+
+            pthread_create(&tids[i], NULL, funcion, posicion);
+            //if(iguales(A, cantidad_numeros) && i == cantidad_numeros - 1){
+            //    i = 0;
+            //}
+        }
+        
+        //Esperar a que terminen las hebras
+        for(int i=0;i<cantidad_numeros;i++){
+            pthread_join(tids[i], NULL);
+        }
+        listo = iguales(A, cantidad_numeros);
+
+    }
 
     // Guardar arreglo final.
     if(archivosalida != NULL){
@@ -137,8 +217,10 @@ int main(int argc, char *argv[])
         vaciar_archivo(archivosalida); //Se limpia el archivo de salida, si es que existe
         guardar_en_archivo(archivosalida, A, cantidad_numeros);
     }
-
     
+    free(A);
+    pthread_mutex_destroy(&lock);
+
     printf("\n*****************FIN DEL PROGRAMA*****************\n");
     return 0;
 }
